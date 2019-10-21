d1f07fa329bbd850f4b2ea27364bf475965a5d0e
Jackson Bradshaw <jacksonbradshaw1@gmail.com>
2019-10-21 13:31:28 -0500

    Allocated code sections for abstract methods

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -1,5 +1,27 @@
 
 public class DateTimeOne extends MesoDateTimeOneAbstract
 {
+   public int getValueOfSecond() {
+	   return 0;
+   }
    
+   public void dateTimeNow() {
+	   
+   }
+   
+   public void sleepForFiveSec() {
+	   
+   }
+   
+   public void dateTimeOfOtherCity() {
+		
+   }
+   
+   public void dateTimeDifferentZone() {
+	
+   }
+   
+   public void timeZoneHashMap() {
+	   
+   }
 }
