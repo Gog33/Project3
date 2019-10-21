c8b10b0dd89c8a79db732b7601745e394844dc4d
Jackson Bradshaw <jacksonbradshaw1@gmail.com>
2019-10-21 13:48:50 -0500

    Add secondValue, got value, complete getter

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -2,14 +2,15 @@ import java.util.Calendar;
 public class DateTimeOne extends MesoDateTimeOneAbstract
 {
 	Calendar calendar;
-	
+	int secondValue;
 	
 	public DateTimeOne() {
-		calendar = Calendar.getInstance();
+		this.calendar = Calendar.getInstance();
+		secondValue = calendar.get(Calendar.SECOND);
 	}
 	
 	public int getValueOfSecond() {
-	   return 0;
+	   return secondValue;
    }
    
    public void dateTimeNow() {
