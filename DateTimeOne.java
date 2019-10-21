cfca842e881dd9b09a4f65610da418ae4f8d1931
Jackson Bradshaw <jacksonbradshaw1@gmail.com>
2019-10-21 14:23:55 -0500

    Improved dateTimeNow method

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -1,4 +1,8 @@
 import java.util.Calendar;
+import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 public class DateTimeOne extends MesoDateTimeOneAbstract
 {
 	Calendar calendar;
@@ -12,11 +16,11 @@ public class DateTimeOne extends MesoDateTimeOneAbstract
    }
    
    public void dateTimeNow() {
-	   String printFormat = "Current Date/Time: " + 
-			   calendar.get(Calendar.MONTH) + "/" + calendar.get(Calendar.DAY_OF_MONTH) +
-			   "/" + calendar.get(Calendar.YEAR) + " " + calendar.get(Calendar.HOUR) + ":" + 
-			   calendar.get(Calendar.MINUTE) + " " + calendar.get(Calendar.AM_PM);
-	   System.out.println(printFormat);
+	   Date date = calendar.getTime();
+	   LocalDateTime ldt = LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+	   DateTimeFormatter dtFormat = DateTimeFormatter.ofPattern("mm/dd/yyyy hh:mm aa");
+
+	   System.out.println("Current Date/Time: " + dtFormat.format(ldt));
    }
    
    public void sleepForFiveSec() {
