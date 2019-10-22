6eb65fa43c21e0d292715072c65ffa8d1c2c3bf0
Jackson Bradshaw <jacksonbradshaw1@gmail.com>
2019-10-21 23:12:05 -0500

    Complete dateTimeOfOtherCity

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -1,7 +1,6 @@
-import java.util.Calendar;
-import java.util.Date;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 public class DateTimeOne extends MesoDateTimeOneAbstract
 {
@@ -17,8 +16,10 @@ public class DateTimeOne extends MesoDateTimeOneAbstract
    
    public void dateTimeNow() {
 	   DateTimeFormatter dtFormat = DateTimeFormatter.ofPattern("mm/dd/yyyy hh:mm aa");
-
-	   System.out.println("Current Date/Time: " + dtFormat.format(dateTime));
+	   LocalDateTime printDate = dateTime;
+	   if (printDate.getHour() > 11)
+		   printDate.minusHours(12);
+	   System.out.println("Current Date/Time: " + dtFormat.format(printDate));
    }
    
    public void sleepForFiveSec() {
@@ -26,7 +27,15 @@ public class DateTimeOne extends MesoDateTimeOneAbstract
    }
    
    public void dateTimeOfOtherCity() {
-		String printFormat = "Time on Server: ";
+		DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("hh:mm");
+	   	String printFormat = "Time on Server: " + timeFormat.format(dateTime) + "\n";
+	   	ZonedDateTime greenwichTime = dateTime.atZone(ZoneId.of("GMT"));
+	   	printFormat += "GMT: " + timeFormat.format(greenwichTime) + "\n";
+	   	ZonedDateTime bangladeshTime = dateTime.atZone(ZoneId.of("BST"));
+	   	printFormat += "BST (90E): " + timeFormat.format(bangladeshTime) + "\n";
+	   	ZonedDateTime centralTime = dateTime.atZone(ZoneId.of("CST"));
+	   	printFormat += "CST (90W): " + timeFormat.format(centralTime) + "\n";
+	   	
 		System.out.println(printFormat);
    }
    
