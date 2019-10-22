0cfed9fddfb27e1ea69611979df232d41e24b936
Jackson Bradshaw <jacksonbradshaw1@gmail.com>
2019-10-22 14:31:48 -0500

    Add constructor

--- DateTimeTwo.java
+++ DateTimeTwo.java
@@ -1,6 +1,11 @@
-
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedHashMap;
 public class DateTimeTwo {
+	LocalDateTime dateTime;
 	
-	
+	public DateTimeTwo() {
+		dateTime = LocalDateTime.now();
+	}
 	
 }
