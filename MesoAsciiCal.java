3db16435b2d40d3b6e76f9786ba5ca303643b9b3
Jackson Bradshaw <jacksonbradshaw1@gmail.com>
2019-10-23 13:00:12 -0500

    Add constructor & spot for abstract calAverage

--- MesoAsciiCal.java
+++ MesoAsciiCal.java
@@ -1,5 +1,12 @@
 
-public class MesoAsciiCal extends MesoAsciiAbstract 
-{
-   
+public class MesoAsciiCal extends MesoAsciiAbstract {
+	MesoStation station;
+	
+	public MesoAsciiCal(MesoStation station) {
+		this.station = station;
+	}
+	
+	public int calAverage() {
+	   return 0;
+   }
 }
