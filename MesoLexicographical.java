8d70771210f92cb3bc0ed27ca6b8b02e5992bbee
Jackson Bradshaw <jacksonbradshaw1@gmail.com>
2019-10-23 14:10:03 -0500

    Space for constructor and sortedMap method

--- MesoLexicographical.java
+++ MesoLexicographical.java
@@ -1,5 +1,12 @@
-
+import java.util.HashMap;
+import java.util.Map;
 public class MesoLexicographical extends MesoSortedAbstract
 {
-
+	public MesoLexicographical(HashMap<String, Integer> asciiVal) {
+		
+	}
+	
+	public Map<String, Integer> sortedMap(HashMap<String, Integer> unsorted) {
+		return unsorted;
+	}
 }
