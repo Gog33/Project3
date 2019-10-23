fbc355213a4d3b4c864695f777a8750df43e230d
Jackson Bradshaw <jacksonbradshaw1@gmail.com>
2019-10-23 13:06:29 -0500

    Complete calAverage method

--- MesoAsciiCal.java
+++ MesoAsciiCal.java
@@ -7,6 +7,16 @@ public class MesoAsciiCal extends MesoAsciiAbstract {
 	}
 	
 	public int calAverage() {
-	   return 0;
-   }
+		double asciiAverage = 0.0;
+		for (int i = 0; i < station.getStID().length(); ++i) {
+			asciiAverage += (int) station.getStID().charAt(i);
+		}
+		asciiAverage = asciiAverage / station.getStID().length();
+
+		if (asciiAverage - Math.floor(asciiAverage) < 0.5) {
+			return (int) Math.floor(asciiAverage);
+		} else {
+			return (int) Math.ceil(asciiAverage);
+		}
+	}
 }
