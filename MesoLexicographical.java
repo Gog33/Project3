e47ebe7fb0f5c537435476966b8cc010cdf33e1d
Jackson Bradshaw <jacksonbradshaw1@gmail.com>
2019-10-23 14:24:32 -0500

    Complete MesoLexicographical constructor and sortedMap

--- MesoLexicographical.java
+++ MesoLexicographical.java
@@ -1,12 +1,20 @@
+import java.util.TreeMap;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 public class MesoLexicographical extends MesoSortedAbstract
 {
 	public MesoLexicographical(HashMap<String, Integer> asciiVal) {
+		Map<String, Integer> sortedMap = sortedMap(asciiVal);
 		
+		for (Entry<String, Integer> entry : sortedMap.entrySet()) {
+			System.out.println(entry.getKey() + " " + entry.getValue());
+		}
 	}
 	
 	public Map<String, Integer> sortedMap(HashMap<String, Integer> unsorted) {
-		return unsorted;
+		TreeMap<String, Integer> sorted = new TreeMap<String, Integer>();
+		sorted.putAll(unsorted);
+		return sorted;
 	}
 }
