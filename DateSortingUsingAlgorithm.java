06656b3bd63dcd8733111fc5b3734a46b1b9e637
Jackson Bradshaw <jacksonbradshaw1@gmail.com>
2019-10-23 14:42:20 -0500

    Complete readDates method and constructor

--- DateSortingUsingAlgorithm.java
+++ DateSortingUsingAlgorithm.java
@@ -1,8 +1,16 @@
-
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 public class DateSortingUsingAlgorithm {
+	HashMap<LocalDate, Integer> datesMap;
 	
-	public DateSortingUsingAlgorithm() {
-		
+	public DateSortingUsingAlgorithm() throws IOException {
+		datesMap = new HashMap<LocalDate, Integer>();
+		readDates();
 	}
 	
 	public void dateHashMapSortedDescending() {
@@ -11,6 +19,22 @@ public class DateSortingUsingAlgorithm {
 	
 	public void dateHashMapSorted() {
 		
+	}
+	
+	public void readDates() throws IOException {
+		DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		BufferedReader br = new BufferedReader(new FileReader("SortingDates.txt"));
+		
+		String input;
+		Integer counter = 0;
+		while ((input = br.readLine()) != null) {
+			++counter;
+			input.trim();
+			LocalDate inputDate = LocalDate.parse(input, inputFormat);
+			datesMap.put(inputDate, counter);
+		}
+		
+		br.close();
 	}
 	
 }
