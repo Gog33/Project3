d553bef55f73182d10a2b12ae57dc04f0c63b71b
Jackson Bradshaw <jacksonbradshaw1@gmail.com>
2019-10-23 13:15:01 -0500

    Create constructor

--- MesoEquivalent.java
+++ MesoEquivalent.java
@@ -1,4 +1,10 @@
 
 public class MesoEquivalent {
+	MesoStation stid;
+	int asciiAverage;
 	
+	public MesoEquivalent(String stid) {
+		this.stid = new MesoStation(stid);
+		asciiAverage = (new MesoAsciiCal(this.stid)).calAverage();
+	}
 }
